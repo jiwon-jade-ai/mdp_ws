869960eeff4d09fec2424923239e0231981b684c
root <justina3587@gmail.com>
2022-11-18 21:37:14 +0000

    added some thicc ass parenthesis around Float64 to all the default constructor. big brain. amazing

--- src/seg_pkg/scripts/inf_class.py
+++ src/seg_pkg/scripts/inf_class.py
@@ -18,21 +18,22 @@ MODEL_PATH = "Unet-diagdataset.pt"
 model = torch.load(MODEL_PATH)
 image_topic =  "/zed2/zed_node/rgb/image_rect_color"
 road_percentage_pub = rospy.Publisher('cv_nav/road_percentage', Float64, queue_size=1)
-road_percentage_msg = Float64
+road_percentage_msg = Float64()
 
-def callback_image(self, msg):
+def callback_image(msg):
     global percentage
     global road_percentage_pub
     global road_percentage_msg
+    global model
     print("Received an image!")
     cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
     image_resize = cv2.resize(cv2_img, (640, 384))
-    pred_mask = predict_image_mask_miou(self.model, image_resize)
+    pred_mask = predict_image_mask_miou(model, image_resize)
     cimage = pred_mask[248:328, 35:385] / 85 # sidewalk is 85
     count = (cimage == 1.0).sum()
     percentage = float(count / np.array(cimage).size * 100)
-    road_percentage_msg.data = self.percentage
-    road_percentage_pub.publish(self.road_percentage_msg)
+    road_percentage_msg.data = percentage
+    road_percentage_pub.publish(road_percentage_msg)
 
 
 def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
@@ -52,7 +53,7 @@ def predict_image_mask_miou(model, image, mean=[0.485, 0.456, 0.406], std=[0.229
 
 if __name__ == '__main__':
     rospy.init_node('segmenation_node')
-    global image_topic
+    #global image_topic
     image_subscriber = rospy.Subscriber(image_topic, Image, callback_image)
     rospy.spin()
 
